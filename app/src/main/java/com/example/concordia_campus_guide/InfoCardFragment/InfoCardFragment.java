f9f8b78393396bd96201566f773a8a009d4801b6
Thomas Gauvin <thomasgauvin.cu@gmail.com>
2020-02-24 16:29:31 -0500

    #5- Added basic info-card layout, added onclick functionality when building (polygon) is clicked

--- app/src/main/java/com/example/concordia_campus_guide/InfoCardFragment/InfoCardFragment.java
+++ app/src/main/java/com/example/concordia_campus_guide/InfoCardFragment/InfoCardFragment.java
@@ -1,7 +1,23 @@
 package com.example.concordia_campus_guide.InfoCardFragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.concordia_campus_guide.R;
 
 // This class is the view (UI) related things
 public class InfoCardFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.info_card_fragment, container, false);
+    }
+
 }
--- app/src/main/java/com/example/concordia_campus_guide/MainActivity.java
+++ app/src/main/java/com/example/concordia_campus_guide/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.concordia_campus_guide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.concordia_campus_guide.InfoCardFragment.InfoCardFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    FragmentTransaction fragmentTransaction;
+
+    InfoCardFragment infoCardFragment = new InfoCardFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+    }
+
+    public void showInfoCard(){
+        fragmentTransaction.add(R.id.info_card_frame, infoCardFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void hideInfoCard(){
+        fragmentTransaction.remove(infoCardFragment);
+        fragmentTransaction.commit();
     }
 }
