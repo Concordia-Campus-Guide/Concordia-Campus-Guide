ec5951ec28a44076221c1c1434f7c8061559a97f
Hambrsoom <hampico1234@gmail.com>
2020-02-29 13:18:29 -0500

    #21 : refactored the code for marker

--- app/src/main/java/com/example/concordia_campus_guide/LocationFragment/LocationFragmentViewModel.java
+++ app/src/main/java/com/example/concordia_campus_guide/LocationFragment/LocationFragmentViewModel.java
@@ -1,12 +1,14 @@
 package com.example.concordia_campus_guide.LocationFragment;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 
 import androidx.lifecycle.ViewModel;
 
-import com.example.concordia_campus_guide.BuildingCode;
 import com.example.concordia_campus_guide.R;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -16,6 +18,7 @@ import com.google.maps.android.geojson.GeoJsonLayer;
 import com.google.maps.android.geojson.GeoJsonPolygonStyle;
 import org.json.JSONException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static java.lang.Double.parseDouble;
 
@@ -36,7 +39,7 @@ public class LocationFragmentViewModel extends ViewModel {
      */
     public GeoJsonLayer loadPolygons(GoogleMap map, Context applicationContext){
         GeoJsonLayer layer = initLayer(map, applicationContext);
-        setPolygonStyle(layer,map);
+        setPolygonStyle(layer,map, applicationContext);
         return  layer;
     }
 
@@ -64,33 +67,52 @@ public class LocationFragmentViewModel extends ViewModel {
      * @param layer the GeoJson layer containing features to style.
      * @param map the google map where layer will be displayed and markers will be added.
      */
-    private void setPolygonStyle(GeoJsonLayer layer, GoogleMap map){
+    private void setPolygonStyle(GeoJsonLayer layer, GoogleMap map, Context context){
         for (GeoJsonFeature feature : layer.getFeatures()){
             feature.setPolygonStyle(getPolygonStyle());
             String[] coordinates = feature.getProperty("center").split(", ");
             LatLng centerPos = new LatLng(parseDouble(coordinates[1]), parseDouble(coordinates[0]));
-            addMarker(map, centerPos, Enum.valueOf(BuildingCode.class, feature.getProperty("code")));
+            addMarker(map, centerPos, feature.getProperty("code"), context);
         }
-        int x =213;
     }
 
     /**
      * The purpose of this method is to add a marker on the specified building.
      * @param map is the map used in our application.
      * @param centerPos is the latitude and longitude of the building's center
-     * @param buildingCode is the Building on which the method will add a marker
+     * @param buildingLabel is the Building on which the method will add a marker
      */
-    private void addMarker(GoogleMap map, LatLng centerPos, BuildingCode buildingCode) {
+    private void addMarker(GoogleMap map, LatLng centerPos, String buildingLabel, Context context) {
         Marker marker = map.addMarker(
                 new MarkerOptions()
                         .position(centerPos)
-                        .icon(BitmapDescriptorFactory.fromResource(getIcon(buildingCode)))
+                        .icon(setupMarker(buildingLabel,context))
                         .flat(true)
                         .anchor(0.5f,0.5f)
                         .alpha(0.90f)
-                        .title(buildingCode.toString())
         );
-        marker.setTag(buildingCode);
+        marker.setTag(buildingLabel);
+    }
+
+    /**
+     * The purpose of this method is setup the style of the marker.
+     * @param buildingLabel the label of the building
+     * @param context the context of the LocationFragment
+     * @return it will BitmapDescriptor object to use it as an icon for the marker on the map.
+     */
+    private BitmapDescriptor setupMarker(String buildingLabel, Context context) {
+        int height = 150;
+        int width = 150;
+        InputStream deckFile = null;
+        try {
+            deckFile = context.getAssets().open("BuildingLabels/" + buildingLabel.toLowerCase()+".png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Bitmap b = BitmapFactory.decodeStream(deckFile);
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor smallMarkerIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+        return  smallMarkerIcon;
     }
 
     /**
@@ -106,79 +128,5 @@ public class LocationFragmentViewModel extends ViewModel {
         return geoJsonPolygonStyle;
     }
 
-    /**
-     * @param buildingCode it represents which building we will be covering
-     * @return Int of drawable resource's bitmap representation
-     */
-    public int getIcon(BuildingCode buildingCode){
-        switch (buildingCode){
-            case H:
-                return R.drawable.h;
-            case LB:
-                return R.drawable.lb;
-            case CJ:
-                return R.drawable.cj;
-            case AD:
-                return R.drawable.ad;
-            case CC:
-                return R.drawable.cc;
-            case EV:
-                return R.drawable.ev;
-            case FG:
-                return R.drawable.fg;
-            case GM:
-                return R.drawable.gm;
-            case MB:
-                return R.drawable.mb;
-            case FB:
-                return R.drawable.fb;
-            case SP:
-                return R.drawable.sp;
-            case BB:
-                return R.drawable.bb;
-            case FC:
-                return R.drawable.fc;
-            case DO:
-                return R.drawable.dome;
-            case GE:
-                return R.drawable.ge;
-            case HA:
-                return R.drawable.ha;
-            case HB:
-                return R.drawable.hb;
-            case HC:
-                return R.drawable.hc;
-            case BH:
-                return R.drawable.bh;
-            case JR:
-                return R.drawable.jr;
-            case PC:
-                return R.drawable.pc;
-            case PS:
-                return R.drawable.ps;
-            case PT:
-                return R.drawable.pt;
-            case PY:
-                return R.drawable.py;
-            case QA:
-                return R.drawable.qa;
-            case RA:
-                return R.drawable.ra;
-            case RF:
-                return R.drawable.rf;
-            case SC:
-                return R.drawable.sc;
-            case SH:
-                return R.drawable.sh;
-            case SI:
-                return R.drawable.si;
-            case TA:
-                return R.drawable.ta;
-            case VE:
-                return R.drawable.ve;
-            case VL:
-                return R.drawable.vl;
-        }
-        return  -1;
-    }
+
 }
