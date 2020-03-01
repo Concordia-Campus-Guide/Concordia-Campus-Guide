b23f6a98f6cb47a68a14af7a8cd5db34febef009
Thomas Gauvin <thomasgauvin.cu@gmail.com>
2020-03-01 15:19:03 -0500

    #38 Added search page activity

---
+++ app/src/main/java/com/example/concordia_campus_guide/Activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.concordia_campus_guide.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.concordia_campus_guide.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_activity);
+    }
+}
