c01237133ad22cbb713a26ab985b7dc962addd22
ferdousara-parvin <parvin.ferdousara@gmail.com>
2020-03-01 08:48:55 -0500

    #5: trying to geti ntegration test working

--- app/src/androidTest/java/com/example/concordia_campus_guide/InfoCardFragmentIntegrationTest.java
+++ app/src/androidTest/java/com/example/concordia_campus_guide/InfoCardFragmentIntegrationTest.java
@@ -1,14 +1,12 @@
 package com.example.concordia_campus_guide;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +15,17 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 @RunWith(AndroidJUnit4.class)
 public class InfoCardFragmentIntegrationTest {
 
+    @Before
+    public void init() {
+
+    }
+
     @Test
     public void testMarker() throws UiObjectNotFoundException {
         UiDevice device = UiDevice.getInstance(getInstrumentation());
         UiObject marker = device.findObject(new UiSelector().descriptionContains("EV"));
         marker.click();
+//        UiObject button = device.findObject(new UiSelector().descriptionContains("Directions"));
+//        button.click();
     }
 }
