368b28a0b54e6f09e395ac29b8fa83212285f14c
ferdousara-parvin <parvin.ferdousara@gmail.com>
2020-03-01 09:58:15 -0500

    #5: fixed issue with sgw and loyola btn

--- app/src/androidTest/java/com/example/concordia_campus_guide/InfoCardFragmentIntegrationTest.java
+++ app/src/androidTest/java/com/example/concordia_campus_guide/InfoCardFragmentIntegrationTest.java
@@ -1,31 +1,141 @@
 package com.example.concordia_campus_guide;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.concordia_campus_guide.Activities.MainActivity;
+import com.example.concordia_campus_guide.InfoCardFragment.InfoCardFragment;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
 import androidx.test.runner.AndroidJUnit4;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class InfoCardFragmentIntegrationTest {
 
+    private UiDevice device;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION");
+
     @Before
     public void init() {
-
+        device = UiDevice.getInstance(getInstrumentation());
     }
+    /**
+     * The purpose of this test is to validate the existance of EV marker
+     * on the screen once opening the application.
+     * @throws UiObjectNotFoundException
+     */
 
     @Test
-    public void testMarker() throws UiObjectNotFoundException {
-        UiDevice device = UiDevice.getInstance(getInstrumentation());
+    public void correctInfoCardPopsUpWhenSpecificMarkerIsClickedTest() throws UiObjectNotFoundException, InterruptedException {
         UiObject marker = device.findObject(new UiSelector().descriptionContains("EV"));
         marker.click();
-//        UiObject button = device.findObject(new UiSelector().descriptionContains("Directions"));
-//        button.click();
+        Thread.sleep(1500);
+
+        ViewInteraction scrollView = onView(
+                allOf(withId(R.id.info_card),
+                        childAtPosition(
+                                allOf(withId(R.id.info_card_coordinator_layout),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        scrollView.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.info_card_title), withText("Engineering, Computer Science and Visual Arts Integrated Complex"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.info_card_frame),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Engineering, Computer Science and Visual Arts Integrated Complex")));
+
+        UiObject collapsedCard = device.findObject(new UiSelector().className(InfoCardFragment.class));
+        collapsedCard.swipeUp(40);
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.building_address), withText("1515 Ste-Catherine West"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        1),
+                                1),
+                        isDisplayed()));
+        textView2.check(matches(withText("1515 Ste-Catherine West")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.departments_list), withText("Gina Cody School of Engineering and Computer Science, Electrical and Computer Engineering, Building, Civil and Environmental Engineering, Mechanical, Industrial and Aerospace Engineering, Design and Computation Arts, Faculty of Fine Arts, Studio Arts, Art Education, Art History, Contemporary Dance, Recreation and Athletics, Centre for Zero Energy Building Studies, Centre for Pattern Recognition and Machine Intelligence (CENPARMI), Center for Composites (CONCOM)"),
+                        childAtPosition(
+                                allOf(withId(R.id.departments),
+                                        childAtPosition(
+                                                withId(R.id.description),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        textView3.check(matches(withText("Gina Cody School of Engineering and Computer Science, Electrical and Computer Engineering, Building, Civil and Environmental Engineering, Mechanical, Industrial and Aerospace Engineering, Design and Computation Arts, Faculty of Fine Arts, Studio Arts, Art Education, Art History, Contemporary Dance, Recreation and Athletics, Centre for Zero Energy Building Studies, Centre for Pattern Recognition and Machine Intelligence (CENPARMI), Center for Composites (CONCOM)")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.services_list), withText("LeGym, FOFA Gallery"),
+                        childAtPosition(
+                                allOf(withId(R.id.services),
+                                        childAtPosition(
+                                                withId(R.id.description),
+                                                2)),
+                                1),
+                        isDisplayed()));
+        textView4.check(matches(withText("LeGym, FOFA Gallery")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
