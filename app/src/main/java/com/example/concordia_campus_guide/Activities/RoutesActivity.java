77c38efd305623c17b7a7222f6804a7e74003b1f
Caren <karen.rizk1997@gmail.com>
2020-03-14 17:10:15 -0400

    #95 added filter buttons in route_activity

--- app/src/main/java/com/example/concordia_campus_guide/Activities/RoutesActivity.java
+++ app/src/main/java/com/example/concordia_campus_guide/Activities/RoutesActivity.java
@@ -1,11 +1,13 @@
 package com.example.concordia_campus_guide.Activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
