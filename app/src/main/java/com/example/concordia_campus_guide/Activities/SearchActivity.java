205bca76811822a25c020df4af2327b4ad961006
narra <narra_pangan@hotmail.com>
2020-03-16 16:26:19 -0400

    #17: Temporarily commented out searchResult. Still need to find a way to get the event's location's display name.

--- app/src/main/java/com/example/concordia_campus_guide/Activities/SearchActivity.java
+++ app/src/main/java/com/example/concordia_campus_guide/Activities/SearchActivity.java
@@ -5,28 +5,39 @@ import android.location.Location;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Group;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.concordia_campus_guide.Adapters.PlaceToSearchResultAdapter;
+import com.example.concordia_campus_guide.Fragments.CalendarFragment.CalendarFragment;
 import com.example.concordia_campus_guide.Global.SelectingToFromState;
 import com.example.concordia_campus_guide.Models.Building;
+import com.example.concordia_campus_guide.Models.CalendarEvent;
 import com.example.concordia_campus_guide.Models.Floor;
 import com.example.concordia_campus_guide.Models.MyCurrentPlace;
 import com.example.concordia_campus_guide.Models.Place;
 import com.example.concordia_campus_guide.Models.RoomModel;
 import com.example.concordia_campus_guide.R;
 
+import java.util.zip.Inflater;
+
 public class SearchActivity extends AppCompatActivity {
 
-    ListView searchResults;
+//    ListView searchResults;
     EditText searchText;
+    View nextClassRow;
+    Fragment nextClassText;
     SearchActivityViewModel mViewModel;
 
     private PlaceToSearchResultAdapter adapter;
@@ -38,13 +49,15 @@ public class SearchActivity extends AppCompatActivity {
         //setting up activity
         setContentView(R.layout.search_activity);
         setBackButtonOnClick();
-        searchResults = (ListView) findViewById(R.id.searchResults);
+//        searchResults = (ListView) findViewById(R.id.searchResults);
         searchText = (EditText) findViewById(R.id.searchText);
         mViewModel = ViewModelProviders.of(this).get(SearchActivityViewModel.class);
+        nextClassRow = (View) findViewById(R.id.view_container);
+        nextClassText = (Fragment) getSupportFragmentManager().findFragmentById(R.id.next_class_text);
 
         //android adapter for list view
         adapter = new PlaceToSearchResultAdapter(this, R.layout.list_item_layout, mViewModel.getAllPlaces());
-        searchResults.setAdapter(adapter);
+//        searchResults.setAdapter(adapter);
 
         //search results filtering according to search text
         searchText.addTextChangedListener(new TextWatcher(){
@@ -59,11 +72,23 @@ public class SearchActivity extends AppCompatActivity {
             public void afterTextChanged(Editable editable){ }
         });
 
-        searchResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        searchResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Place place = adapter.getItem(position);
+//                openRoutesPage(place);
+//            }
+//        });
+
+        nextClassRow.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Place place = adapter.getItem(position);
-                openRoutesPage(place);
+            public void onClick(View v) {
+                openRoutesPage(new Place() {
+                    @Override
+                    public String getDisplayName() {
+                        return nextClassText.toString();
+                    }
+                });
             }
         });
     }
