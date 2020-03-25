8418dafac09acccc25dedccdc3ec938f06b9cdbc
narra <narra_pangan@hotmail.com>
2020-03-24 21:56:50 -0400

    #115: Updated unit tests for Coordinates

--- app/src/test/java/com/example/concordia_campus_guide/ModelTests/ListOfCoordinatesTest.java
+++ app/src/test/java/com/example/concordia_campus_guide/ModelTests/ListOfCoordinatesTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertEquals;
 
 public class ListOfCoordinatesTest {
@@ -22,6 +23,22 @@ public class ListOfCoordinatesTest {
         testingList.add(new Coordinates(-73.57921063899994, 45.49707133596979));
         coordinates = new ListOfCoordinates();
     }
+
+    @Test
+    public void equalsTest(){
+        List<Coordinates> testingListEquals = new ArrayList<>();
+        testingListEquals.add(new Coordinates(-73.57907921075821, 45.49702057370776));
+        testingListEquals.add(new Coordinates(-73.57907921075821, 45.49702057370776));
+        assertTrue(testingListEquals.get(0).equals(testingListEquals.get(1)));
+
+    }
+
+    @Test
+    public void toListDoubleTest(){
+    }
+
+//      assertEquals(testingList.get(0).getLatitude(), testingList.get(1).getLatitude());
+//        assertEquals(testingList.get(0).getLongitude(), testingList.get(1).getLongitude());
 
     @Test
     public void getAndSetCoordinatesTest(){
