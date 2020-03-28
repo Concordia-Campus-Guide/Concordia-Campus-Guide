44c60ff8260a7ce9730d85e92f5afbbb53f3e83f
Hambrsoom <hampico1234@gmail.com>
2020-03-27 21:06:13 -0400

    #97: uncommented the InfoCardIntegration test class

--- app/src/androidTest/java/com/example/concordia_campus_guide/InstrumentalTests/CalendarViewModelInstrumentalTest.java
+++ app/src/androidTest/java/com/example/concordia_campus_guide/InstrumentalTests/CalendarViewModelInstrumentalTest.java
@@ -18,45 +18,45 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class CalendarViewModelInstrumentalTest {
-//    private CalendarViewModel mViewModel;
-//    private Context appContext;
-//    private Application application;
-//    private Cursor cursor;
-//
-//    @Rule
-//    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
-//
-//    @Before
-//    public void init(){
-//        application = mActivityRule.getActivity().getApplication();
-//        mViewModel = new CalendarViewModel(application);
-//        cursor = mViewModel.getCalendarCursor();
-//    }
-//
-//    @Test
-//    public void getCalendarEventNoMatch() {
-//        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
-//        assertNull(calendarEvent);
-//    }
-//
-//    @Test
-//    public void getCalendarEventMatchLecture(){
-//        String containsLecture = "Lecture: SOEN 390";
-//        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
-//        assertNotNull(calendarEvent);
-//    }
-//
-//    @Test
-//    public void getCalendarEventMatchTutorial(){
-//        String containsTutorial = "Tutorial: SOEN 390";
-//        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
-//        assertNotNull(calendarEvent);
-//    }
-//
-//    @Test
-//    public void getCalendarEventMatchLab(){
-//        String containsLab = "Lab: COMP 445";
-//        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
-//        assertNotNull(calendarEvent);
-//    }
+    private CalendarViewModel mViewModel;
+    private Context appContext;
+    private Application application;
+    private Cursor cursor;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void init(){
+        application = mActivityRule.getActivity().getApplication();
+        mViewModel = new CalendarViewModel(application);
+        cursor = mViewModel.getCalendarCursor();
+    }
+
+    @Test
+    public void getCalendarEventNoMatch() {
+        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
+        assertNull(calendarEvent);
+    }
+
+    @Test
+    public void getCalendarEventMatchLecture(){
+        String containsLecture = "Lecture: SOEN 390";
+        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
+        assertNotNull(calendarEvent);
+    }
+
+    @Test
+    public void getCalendarEventMatchTutorial(){
+        String containsTutorial = "Tutorial: SOEN 390";
+        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
+        assertNotNull(calendarEvent);
+    }
+
+    @Test
+    public void getCalendarEventMatchLab(){
+        String containsLab = "Lab: COMP 445";
+        CalendarEvent calendarEvent = mViewModel.getCalendarEvent(cursor);
+        assertNotNull(calendarEvent);
+    }
 }
