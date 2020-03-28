6ac12e5e1fb760975f02a7faef0b2c35115d9f12
viveka <viveka98@hotmail.ca>
2020-03-28 19:27:23 -0400

    #11: Handing the case where the current location can be null

--- app/src/main/java/com/example/concordia_campus_guide/Fragments/LocationFragment/LocationFragmentViewModel.java
+++ app/src/main/java/com/example/concordia_campus_guide/Fragments/LocationFragment/LocationFragmentViewModel.java
@@ -300,7 +300,20 @@ public class LocationFragmentViewModel extends ViewModel {
 
     private PriorityQueue<WalkingPoint> getPOIinOrder(List<WalkingPoint> allPOI) {
         PriorityQueue<WalkingPoint> orderedList = new PriorityQueue<>((WalkingPoint p1, WalkingPoint p2) -> {
-            Coordinates currentCoordinates = new Coordinates(currentLocation.getLatitude(), currentLocation.getLongitude());
+            Coordinates currentCoordinates;
+
+            if (currentLocation == null) {
+                //This building has inverted lat/lng in order to us th geojsons.
+                Building hallBuilding = appDatabase.buildingDao().getBuildingByBuildingCode("H");
+                Double currentLat = hallBuilding != null? hallBuilding.getCenterCoordinates().getLatitude() : 0;
+                Double currentLng = hallBuilding != null? hallBuilding.getCenterCoordinates().getLongitude(): 0;
+
+                //Current location should be inversed: lat->lng and lng->lat
+                currentCoordinates = new Coordinates(currentLng, currentLat);
+            } else {
+                currentCoordinates = new Coordinates(currentLocation.getLatitude(), currentLocation.getLongitude());
+            }
+
             double distanceFromO1 = p1.getCoordinate().getEuclideanDistanceFrom(currentCoordinates);
             double distanceFromO2 = p2.getCoordinate().getEuclideanDistanceFrom(currentCoordinates);
 
@@ -312,7 +325,6 @@ public class LocationFragmentViewModel extends ViewModel {
         orderedList.addAll(allPOI);
         return orderedList;
     }
-
 
     LiveData<PriorityQueue<WalkingPoint>> getListOfPOI() {
         return poiList;
