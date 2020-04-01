8db9d303aa4a2bf4ad13be9d63b13a7e7cb70cfe
Hambrsoom <hampico1234@gmail.com>
2020-04-01 15:07:13 -0400

    refactored variable names in the testing classes

--- app/src/test/java/com/example/concordia_campus_guide/HelpersTest/IndoorPathHeuristicTest.java
+++ app/src/test/java/com/example/concordia_campus_guide/HelpersTest/IndoorPathHeuristicTest.java
@@ -5,7 +5,6 @@ import com.example.concordia_campus_guide.Database.Daos.WalkingPointDao;
 import com.example.concordia_campus_guide.Helper.IndoorPathHeuristic;
 import com.example.concordia_campus_guide.Models.Coordinates;
 import com.example.concordia_campus_guide.Models.PointType;
-import com.example.concordia_campus_guide.Models.RoomModel;
 import com.example.concordia_campus_guide.Models.WalkingPoint;
 
 import org.junit.Before;
@@ -24,16 +23,21 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndoorPathHeuristicTest {
-    private IndoorPathHeuristic indoorPathHeuristic;
+    private WalkingPoint classRoomInHBuildingWalkingPoint1;
     private WalkingPoint walkingPoint1;
     private WalkingPoint walkingPoint2;
     private WalkingPoint walkingPoint3;
+    private WalkingPoint classRoomInHBuildingWalkingPoint2;
+    private WalkingPoint entranceHBuildingWalkingPoint;
+    private WalkingPoint elevatorWalkingPointH9;
+    private WalkingPoint entranceMBBuildingWalkingPoint;
     private WalkingPoint walkingPoint4;
-    private WalkingPoint walkingPoint5;
-    private WalkingPoint walkingPoint6;
-    private WalkingPoint walkingPoint7;
-    private WalkingPoint walkingPoint8;
+    private WalkingPoint elevatorWalkingPointH8;
+    private WalkingPoint classRoomInMBBuildingWalkingPoint;
     private List<WalkingPoint> walkingPointList;
+    private WalkingPoint walkingPoint5;
+    private IndoorPathHeuristic indoorPathHeuristic;
+    private WalkingPoint classRoomInHBuildingWalkingPointh8;
 
     @Mock
     AppDatabase mockAppDb;
@@ -46,47 +50,53 @@ public class IndoorPathHeuristicTest {
         MockitoAnnotations.initMocks(this);
         settingUpWalkingPoints();
 
-        walkingPointList = Arrays.asList(walkingPoint1, walkingPoint2, walkingPoint3, walkingPoint4, walkingPoint5, walkingPoint6, walkingPoint6, walkingPoint8);
+        walkingPointList = Arrays.asList(classRoomInHBuildingWalkingPoint1, walkingPoint1, walkingPoint2, walkingPoint3, classRoomInHBuildingWalkingPoint2,
+                entranceHBuildingWalkingPoint, elevatorWalkingPointH9,entranceMBBuildingWalkingPoint, walkingPoint4, classRoomInMBBuildingWalkingPoint);
 
         when(mockAppDb.walkingPointDao()).thenReturn(mockWalkingPointDao);
-        when(mockWalkingPointDao.getAllAccessPointsOnFloor(walkingPoint1.getFloorCode(),PointType.ELEVATOR)).thenReturn(walkingPointList);
+        when(mockWalkingPointDao.getAllAccessPointsOnFloor(classRoomInHBuildingWalkingPoint1.getFloorCode(),PointType.ENTRANCE)).thenReturn(Arrays.asList(classRoomInHBuildingWalkingPoint1));
         when(mockWalkingPointDao.getAll()).thenReturn(walkingPointList);
         indoorPathHeuristic = new IndoorPathHeuristic(mockAppDb);
     }
 
     private void settingUpWalkingPoints(){
-        walkingPoint1 = new WalkingPoint(1,new Coordinates(1, 1), "H-9",new ArrayList<>(Arrays.asList(new Integer[]{2,3,4})), PointType.CLASSROOM, "937");
-        walkingPoint2 = new WalkingPoint(2,new Coordinates(2,2), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{1,3})),PointType.NONE,"NONE");
-        walkingPoint3 = new WalkingPoint(3,new Coordinates(1,2), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{2,4})),PointType.NONE,"NONE");
-        walkingPoint4 = new WalkingPoint(4,new Coordinates(-1, -2), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{1,3,5,6})),PointType.CLASSROOM,"921");
-        walkingPoint5 = new WalkingPoint(5,new Coordinates(2, 3), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{4,8})),PointType.ENTRANCE,"entrance");
-        walkingPoint6 = new WalkingPoint(6,new Coordinates(2, 1), "H-8", new ArrayList<>(Arrays.asList(new Integer[]{4,7})),PointType.ELEVATOR,"elevator");
-        walkingPoint7 = new WalkingPoint(7,new Coordinates(4, -1), "H-8", new ArrayList<>(Arrays.asList(new Integer[]{6})),PointType.CLASSROOM,"832");
-        walkingPoint8 = new WalkingPoint(8,new Coordinates(3, 1), "MB-2", new ArrayList<>(Arrays.asList(new Integer[]{5})),PointType.CLASSROOM,"S-23");
+        classRoomInHBuildingWalkingPoint1 = new WalkingPoint(1,new Coordinates(1, 1), "H-9",new ArrayList<>(Arrays.asList(new Integer[]{2,3,4})), PointType.CLASSROOM, "937");
+        walkingPoint1 = new WalkingPoint(2,new Coordinates(2,2), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{1,3,7})),PointType.NONE,"NONE");
+        walkingPoint2 = new WalkingPoint(3,new Coordinates(2,-2.5), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{2,4,5})),PointType.NONE,"NONE");
+        walkingPoint3 = new WalkingPoint(4,new Coordinates(1, 1.5), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{1,3,5,6})),PointType.NONE,"NONE");
+        classRoomInHBuildingWalkingPoint2 = new WalkingPoint(5,new Coordinates(5, 5), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{3,4})),PointType.CLASSROOM,"921");
+        entranceHBuildingWalkingPoint = new WalkingPoint(6,new Coordinates(1.2, 2), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{4,8})),PointType.ENTRANCE,"ENTRANCE");
+        elevatorWalkingPointH9 = new WalkingPoint(7,new Coordinates(1, 3), "H-9", new ArrayList<>(Arrays.asList(new Integer[]{2,11})),PointType.ELEVATOR,"elevators-1");
+        elevatorWalkingPointH8 = new WalkingPoint(11,new Coordinates(1, 3), "H-8", new ArrayList<>(Arrays.asList(new Integer[]{7,12})),PointType.ELEVATOR,"elevators-1");
+        entranceMBBuildingWalkingPoint = new WalkingPoint(8, new Coordinates(2.4,3.2),"MB-S2",new ArrayList<>(Arrays.asList(new Integer[]{7,9})),PointType.ENTRANCE, "ENTRANCE");
+        walkingPoint4 = new WalkingPoint(9,new Coordinates(-2.4,2.2),"MB-S2",new ArrayList<>(Arrays.asList(new Integer[]{8,10})),PointType.NONE, "NONE");
+        classRoomInMBBuildingWalkingPoint = new WalkingPoint(10,new Coordinates(2.45,-2.2),"MB-S2",new ArrayList<>(Arrays.asList(new Integer[]{9})),PointType.CLASSROOM, "279");
+        walkingPoint5 = new WalkingPoint(12,new Coordinates(2, 3), "H-8", new ArrayList<>(Arrays.asList(new Integer[]{11,13})),PointType.NONE,"NONE");
+        classRoomInHBuildingWalkingPointh8 =new WalkingPoint(13,new Coordinates(1, 1), "H-8",new ArrayList<>(Arrays.asList(new Integer[]{12})), PointType.CLASSROOM, "837");
     }
 
     @Test
     public void getEuclideanDistanceTest(){
         double expectedValue = 1.4142135623730951;
-        assertEquals(expectedValue, indoorPathHeuristic.getEuclideanDistance(walkingPoint1,walkingPoint2));
+        assertEquals(expectedValue, indoorPathHeuristic.getEuclideanDistance(classRoomInHBuildingWalkingPoint1, walkingPoint1));
     }
 
     @Test
     public void getClosestPointToCurrentPointFromListTest(){
         List<WalkingPoint> closestPoint = new ArrayList<>();
-        closestPoint.add(walkingPoint2);
-        closestPoint.add(walkingPoint4);
-        assertEquals(walkingPoint2, indoorPathHeuristic.getClosestPointToCurrentPointFromList(walkingPoint1,closestPoint));
+        closestPoint.add(walkingPoint1);
+        closestPoint.add(walkingPoint3);
+        assertEquals(walkingPoint3, indoorPathHeuristic.getClosestPointToCurrentPointFromList(classRoomInHBuildingWalkingPoint1,closestPoint));
     }
 
     @Test
     public void getNearestAccessPointForFloorTest() {
         List<WalkingPoint> temp = new ArrayList<>();
-        temp.add(walkingPoint4);
         temp.add(walkingPoint3);
-        when(mockWalkingPointDao.getAllAccessPointsOnFloor(walkingPoint1.getFloorCode(),PointType.CLASSROOM)).thenReturn(temp);
+        temp.add(walkingPoint2);
+        when(mockWalkingPointDao.getAllAccessPointsOnFloor(classRoomInHBuildingWalkingPoint1.getFloorCode(),PointType.CLASSROOM)).thenReturn(temp);
 
-        WalkingPoint walkingPointFound = indoorPathHeuristic.getNearestAccessPointForFloor(walkingPoint1,PointType.CLASSROOM);
+        WalkingPoint walkingPointFound = indoorPathHeuristic.getNearestAccessPointForFloor(classRoomInHBuildingWalkingPoint1,PointType.CLASSROOM);
         assertEquals(walkingPoint3,walkingPointFound);
     }
 
@@ -94,18 +104,19 @@ public class IndoorPathHeuristicTest {
     @Test
     public void computeHeuristicSameFloorTest(){
         double expectedHeuristicValue = 1.4142135623730951;
-        assertEquals(expectedHeuristicValue,indoorPathHeuristic.computeHeuristic(walkingPoint1,walkingPoint2));
+        assertEquals(expectedHeuristicValue,indoorPathHeuristic.computeHeuristic(classRoomInHBuildingWalkingPoint1, walkingPoint1));
     }
 
     @Test
     public void computeHeuristicDifferentFloorTest(){
-        double expectedHeuristicValue = 3.605551275463989;
-        assertEquals(expectedHeuristicValue,indoorPathHeuristic.computeHeuristic(walkingPoint1,walkingPoint7));
+        when(mockWalkingPointDao.getAllAccessPointsOnFloor(classRoomInHBuildingWalkingPoint1.getFloorCode(),PointType.ELEVATOR)).thenReturn(Arrays.asList(elevatorWalkingPointH9));
+        double expectedHeuristicValue = 4.0;
+        assertEquals(expectedHeuristicValue,indoorPathHeuristic.computeHeuristic(classRoomInHBuildingWalkingPoint1,classRoomInHBuildingWalkingPointh8));
     }
 
     @Test
     public void computeHeuristicDifferentBuildingsTest(){
-        double expectedHeuristicValue = 2.0;
-        assertEquals(expectedHeuristicValue,indoorPathHeuristic.computeHeuristic(walkingPoint1,walkingPoint8));
+        double expectedHeuristicValue = 3.513189434118235;
+        assertEquals(expectedHeuristicValue,indoorPathHeuristic.computeHeuristic(classRoomInHBuildingWalkingPoint1,classRoomInMBBuildingWalkingPoint));
     }
 }
