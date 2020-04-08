1b663e9ae413aef964626f96669c3cc5557660f3
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2020-04-08 11:18:20 -0400

    #158: renived a todo

--- app/src/main/java/com/example/concordia_campus_guide/Models/MyCurrentPlace.java
+++ app/src/main/java/com/example/concordia_campus_guide/Models/MyCurrentPlace.java
@@ -1,7 +1,6 @@
 package com.example.concordia_campus_guide.Models;
 
 public class MyCurrentPlace extends Place {
-    // TODO: add to strings.xml (find a way to access without context)
     String displayName = "Select location";
 
     public MyCurrentPlace(Double latitude, Double longitude){
