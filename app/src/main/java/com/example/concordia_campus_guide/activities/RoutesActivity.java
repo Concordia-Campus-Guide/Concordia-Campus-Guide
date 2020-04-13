3df3bd746fb5ebb499e2860bac61db1f88b9e5c0
Ferdousara Parvin <parvin.ferdousara@gmail.com>
2020-04-13 17:26:42 -0400

    Update app/src/main/java/com/example/concordia_campus_guide/activities/RoutesActivity.java
    Co-Authored-By: viveanban <viveka98@hotmail.ca>

--- app/src/main/java/com/example/concordia_campus_guide/activities/RoutesActivity.java
+++ app/src/main/java/com/example/concordia_campus_guide/activities/RoutesActivity.java
@@ -141,7 +141,7 @@ public class RoutesActivity extends AppCompatActivity implements DirectionsApiCa
     public void onClickDisability(View v) {
         disabilityButton.setSelected(!disabilityButton.isSelected());
 
-        SharedPreferences sharedPreferences2 = getSharedPreferences(ClassConstants.SHARED_PREFERENCES, MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences(ClassConstants.SHARED_PREFERENCES, MODE_PRIVATE);
         sharedPreferences2.edit().putBoolean(ClassConstants.ACCESSIBILITY_TOGGLE, disabilityButton.isSelected()).commit();
     }
 
